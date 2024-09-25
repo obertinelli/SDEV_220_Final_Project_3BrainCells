ac3c277920bee13184681972d67927e06dc7cf2a
Veloscocity <veloscocity@gmail.com>
2024-09-25 18:24:38 -0400

    initialize django

---
+++ bookstoreinv/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    isbn = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
