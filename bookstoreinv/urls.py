810e4bc7964b22d42c7a5e3fb369eb34ddca68ca
Faraz Fayyaz <farazfayyazwork@gmail.com>
2024-10-09 00:45:19 -0400

    added forms for adding new books, updating current book information, a navigation, and cleaner styling

--- bookstoreinv/urls.py
+++ bookstoreinv/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_screen, name='home_screen'), # main screen link
+    path('book/<str:pk>', views.book_details, name="book_details"), #book detail screen
+    path('book/new/', views.add_book, name="add_book"),
+    path('book/<str:pk>/edit/', views.book_edit, name='book_edit'),
 ]
--- bookstoreinv/views.py
+++ bookstoreinv/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from .models import Book
+from .forms import BookForm
 # from .Inventory_Class import Inventory
 
 # Create your views here.
@@ -8,3 +10,33 @@ from .models import Book
 def home_screen(request):
     books = Book.objects.order_by('title')
     return render(request, 'pages/home_screen.html', {'books': books}) 
+
+# view for book_details
+def book_details(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'pages/book_details.html', {'book': book})
+
+# view for add_book - logic to add new books
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('book_details', pk=post.pk)
+    else:
+        form = BookForm()
+    return render(request, 'pages/add_book.html', {'form': form})
+
+# view for edit_book - logic to edit existing book content
+def book_edit(request, pk):
+    post = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('book_details', pk=post.pk)
+    else:
+        form = BookForm(instance=post)
+    return render(request, 'pages/add_book.html', {'form': form})
